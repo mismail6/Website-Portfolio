9d082b537c99b62a0a9101ca66207835dd738f1e
Muhammad Ismail <mismail6@wisc.edu>
2025-02-05 23:55:03 -0600

    Fix masonry layout bug

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     var grid = document.querySelector('.grid');
-    var msnry = new Masonry(grid, {
-        itemSelector: '.card',
-        columnWidth: '.grid-sizer',
-        percentPosition: true,
-        gutter: 30 // Increased gutter size
+    
+    imagesLoaded(grid, function () {
+        var msnry = new Masonry(grid, {
+            itemSelector: '.card',
+            columnWidth: '.grid-sizer',
+            percentPosition: true,
+            gutter: 30 // Increased gutter size
+        });
     });
 });
